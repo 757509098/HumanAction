35167e2dea3e4bb5d0d062313611edd7ba0bc9cf
hhl <757509098h@gmail.com>
2026-04-15 15:11:42 +0800

    Initial Commit

---
+++ HumanAction/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HumanAction
+//
+//  Created by HHL on 2026/4/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HumanAction/HumanActionApp.swift
@@ -0,0 +1,17 @@
+//
+//  HumanActionApp.swift
+//  HumanAction
+//
+//  Created by HHL on 2026/4/15.
+//
+
+import SwiftUI
+
+@main
+struct HumanActionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
